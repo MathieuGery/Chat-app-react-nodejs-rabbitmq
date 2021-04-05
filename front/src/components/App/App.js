f5428a33e51bb31164278943e3848b57c5460beb
mathieugery <mathieu.gery@epitech.eu>
2021-04-05 22:08:37 +0200

    Create SignUp page

--- front/src/components/App/App.js
+++ front/src/components/App/App.js
@@ -1,6 +1,6 @@
-import '../../App.css';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Login from "../Login";
+import SignUp from "../SignUp";
 
 function App() {
   return (
@@ -8,10 +8,10 @@ function App() {
         <div className="">
           <Switch>
             <Route exact path="/" component={Login}/>
+            <Route exact path="/signup" component={SignUp}/>
           </Switch>
         </div>
       </BrowserRouter>
   );
 }
-
 export default App;
