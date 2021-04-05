e077f7ddbd88bc59dd9360cc0deb42cfc99c3e8a
mathieugery <mathieu.gery@epitech.eu>
2021-04-05 22:37:43 +0200

    Rollback

--- front/src/components/Login/index.js
+++ front/src/components/Login/index.js
@@ -4,32 +4,6 @@ import {Link} from "react-router-dom";
 function Login() {
     return (
         <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-            <div className="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full" aria-hidden="true">
-                <div className="relative h-full max-w-7xl mx-auto">
-                    <svg className="absolute right-full transform translate-y-1/4 translate-x-1/4 lg:translate-x-1/2"
-                         width="404" height="784" fill="none" viewBox="0 0 404 300">
-                        <defs>
-                            <pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20"
-                                     patternUnits="userSpaceOnUse">
-                                <rect x="0" y="0" width="4" height="4" className="text-indigo-200" fill="currentColor"/>
-                            </pattern>
-                        </defs>
-                        <rect width="404" height="784" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"/>
-                    </svg>
-                    <svg
-                        className="absolute left-full transform -translate-y-3/4 -translate-x-1/4 md:-translate-y-1/2 lg:-translate-x-1/2"
-                        width="404" height="784" fill="none" viewBox="0 0 404 784">
-                        <defs>
-                            <pattern id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b" x="0" y="0" width="20" height="20"
-                                     patternUnits="userSpaceOnUse">
-                                <rect x="0" y="0" width="4" height="4" className="text-indigo-200" fill="currentColor"/>
-                            </pattern>
-                        </defs>
-                        <rect width="404" height="784" fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)"/>
-                    </svg>
-                </div>
-            </div>
-
             <div className="sm:mx-auto sm:w-full sm:max-w-md">
                 <svg xmlns="http://www.w3.org/2000/svg" className="mx-auto h-12 w-auto stroke-current text-indigo-500" fill="none" viewBox="0 0 24 24"
                      stroke="currentColor">
