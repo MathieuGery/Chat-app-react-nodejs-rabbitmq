823c11da49b4c855c006ad964c789c617018b759
mathieugery <mathieu.gery@epitech.eu>
2021-04-06 15:23:38 +0200

    Create two rows for chat and rooms

--- front/src/components/Home/index.js
+++ front/src/components/Home/index.js
@@ -34,14 +34,14 @@ function Home() {
 
                 <main className="flex-1 flex overflow-hidden">
                     <div className="flex-1 flex xl:overflow-hidden">
-                        <section aria-labelledby="primary-heading" className="min-w-0 flex-1 h-full flex flex-col overflow-hidden lg:order-last">
+                        <section aria-labelledby="primary-heading" className="min-w-0 flex-1 h-full flex flex-col overflow-hidden lg:order-first">
                             <h1 id="primary-heading" className="sr-only text-black ">Account</h1>
-                            Chat
+                            Rooms
                         </section>
 
                         <aside className="hidden lg:block lg:flex-shrink-0 lg:order-first">
                             <div className="h-full relative flex flex-col w-96 border-r border-gray-200 bg-white">
-                                Conversations
+                                Chat
                             </div>
                         </aside>
                     </div>
