7b8189a5d03d7b59ad1cfc6809a43e757d605963
mathieugery <mathieu.gery@epitech.eu>
2021-04-06 22:44:54 +0200

    Create rooms and chat list section

---
+++ front/src/components/ChatList/index.js
@@ -0,0 +1,95 @@
+function ChatList() {
+    return (
+        <div className="mx-2 overflow-y-auto">
+            <ul className="divide-y divide-gray-200">
+                <li className="px-6 py-5 relative">
+                    <div className="group flex justify-between items-center">
+                        <a href="#" className="-m-1 p-1 block">
+                            <div className="absolute inset-0 group-hover:bg-gray-50" aria-hidden="true"/>
+                            <div className="flex-1 flex items-center min-w-0 relative">
+                    <span className="flex-shrink-0 inline-block relative">
+                      <img className="h-10 w-10 rounded-full"
+                           src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixqx=l28H8Z9jWr&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                           alt=""/>
+                          <span
+                              className="bg-green-400 absolute top-0 right-0 block h-2.5 w-2.5 rounded-full ring-2 ring-white"
+                              aria-hidden="true"/>
+                    </span>
+                                <div className="ml-4 truncate">
+                                    <p className="text-sm font-medium text-gray-900 truncate">Leslie Alexander</p>
+                                    <p className="text-sm text-gray-500 truncate">@lesliealexander</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li className="px-6 py-5 relative">
+                    <div className="group flex justify-between items-center">
+                        <a href="#" className="-m-1 p-1 block">
+                            <div className="absolute inset-0 group-hover:bg-gray-50" aria-hidden="true"/>
+                            <div className="flex-1 flex items-center min-w-0 relative">
+                    <span className="flex-shrink-0 inline-block relative">
+                      <img className="h-10 w-10 rounded-full"
+                           src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixqx=l28H8Z9jWr&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                           alt=""/>
+                          <span
+                              className="bg-green-400 absolute top-0 right-0 block h-2.5 w-2.5 rounded-full ring-2 ring-white"
+                              aria-hidden="true"/>
+                    </span>
+                                <div className="ml-4 truncate">
+                                    <p className="text-sm font-medium text-gray-900 truncate">Leslie Alexander</p>
+                                    <p className="text-sm text-gray-500 truncate">@lesliealexander</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li className="px-6 py-5 relative">
+                    <div className="group flex justify-between items-center">
+                        <a href="#" className="-m-1 p-1 block">
+                            <div className="absolute inset-0 group-hover:bg-gray-50" aria-hidden="true"/>
+                            <div className="flex-1 flex items-center min-w-0 relative">
+                    <span className="flex-shrink-0 inline-block relative">
+                      <img className="h-10 w-10 rounded-full"
+                           src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixqx=l28H8Z9jWr&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                           alt=""/>
+                          <span
+                              className="bg-green-400 absolute top-0 right-0 block h-2.5 w-2.5 rounded-full ring-2 ring-white"
+                              aria-hidden="true"/>
+                    </span>
+                                <div className="ml-4 truncate">
+                                    <p className="text-sm font-medium text-gray-900 truncate">Leslie Alexander</p>
+                                    <p className="text-sm text-gray-500 truncate">@lesliealexander</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li className="px-6 py-5 relative">
+                    <div className="group flex justify-between items-center">
+                        <a href="#" className="-m-1 p-1 block">
+                            <div className="absolute inset-0 group-hover:bg-gray-50" aria-hidden="true"/>
+                            <div className="flex-1 flex items-center min-w-0 relative">
+                    <span className="flex-shrink-0 inline-block relative">
+                      <img className="h-10 w-10 rounded-full"
+                           src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixqx=l28H8Z9jWr&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                           alt=""/>
+                          <span
+                              className="bg-green-400 absolute top-0 right-0 block h-2.5 w-2.5 rounded-full ring-2 ring-white"
+                              aria-hidden="true"/>
+                    </span>
+                                <div className="ml-4 truncate">
+                                    <p className="text-sm font-medium text-gray-900 truncate">Leslie Alexander</p>
+                                    <p className="text-sm text-gray-500 truncate">@lesliealexander</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                {/*<!-- More items... -->*/}
+            </ul>
+        </div>
+    );
+}
+
+export default ChatList;
--- front/src/components/Home/index.js
+++ front/src/components/Home/index.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import SideBarMobile from "../SideBar/SideBarMobile";
 import SideBarDesktop from "../SideBar/SideBarDesktop";
+import ChatList from "../ChatList";
 
 function Home() {
     const [isOpen, setIsOpen] = useState(false);
@@ -34,12 +35,12 @@ function Home() {
 
                 <main className="flex-1 flex overflow-hidden">
                     <div className="flex-1 flex xl:overflow-hidden">
-                        <section aria-labelledby="primary-heading" className="min-w-0 flex-1 h-full flex flex-col overflow-hidden lg:order-first">
+                        <section aria-labelledby="primary-heading" className="min-w-0 max-w-lg h-full flex flex-col overflow-hidden flex-1 lg:order-first">
                             <h1 id="primary-heading" className="sr-only text-black ">Account</h1>
-                            Rooms
+                            <ChatList/>
                         </section>
 
-                        <aside className="hidden lg:block lg:flex-shrink-0 lg:order-first">
+                        <aside className="hidden lg:block lg:flex-shrink-0 flex-1">
                             <div className="h-full relative flex flex-col w-96 border-r border-gray-200 bg-white">
                                 Chat
                             </div>
