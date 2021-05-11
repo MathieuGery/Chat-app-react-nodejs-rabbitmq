335ef105e1a1984fae5fcbdafecd92f55ce04242
mathieugery <mathieu.gery@epitech.eu>
2021-05-11 19:50:30 +0200

    [Fix] - Add chat with rooms

--- back/chat/index.js
+++ back/chat/index.js
@@ -1,54 +1,78 @@
-const io = require("socket.io")(3000, {});
-let amqp = require('amqplib/callback_api');
-const mongoose = require('mongoose');
+let app = require('express')();
+let http = require('http').createServer(app);
+const PORT = process.env.PORT || 5000;
+let io = require('socket.io')(http);
 
-const server = io.listen(5000);
-console.log(`Listening on 5000`);
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://dbUser:H3ljVKmxsVBc4fiu@cluster0.yjsgw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
-let connected_users_db = null;
+let STATIC_CHANNELS = [{
+    name: 'Global chat',
+    participants: 0,
+    id: 1,
+    sockets: []
+}, {
+    name: 'Friendly user',
+    participants: 0,
+    id: 2,
+    sockets: []
+}];
 
-client.connect(function (err, db) {
-    if (err) throw err;
-    let dbo = db.db("myFirstDatabase");
-    connected_users_db = dbo.collection("connected_users");
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+})
+
+
+http.listen(PORT, () => {
+    console.log(`listening on *:${PORT}`);
 });
 
-function setConnectedUser(username) {
-    connected_users_db?.insertOne({name: username}, function (err, res) {
-        if (err) throw err;
+io.on('connection', (socket) => { // socket object may be used to send specific messages to the new connected client
+    console.log('New client connected');
+    socket.emit('connection', null);
+    socket.on('channel-join', id => {
+        console.log('channel join', id);
+        STATIC_CHANNELS.forEach(c => {
+            if (c.id === id) {
+                if (c.sockets.indexOf(socket.id) == (-1)) {
+                    c.sockets.push(socket.id);
+                    c.participants++;
+                    io.emit('channel', c);
+                }
+            } else {
+                let index = c.sockets.indexOf(socket.id);
+                if (index != (-1)) {
+                    c.sockets.splice(index, 1);
+                    c.participants--;
+                    io.emit('channel', c);
+                }
+            }
+        });
+
+        return id;
     });
-}
-
-function removeConnectedUser(username) {
-    connected_users_db?.deleteOne({name: username}, function (err, res) {
-        if (err) throw err;
+    socket.on('send-message', message => {
+        io.emit('message', message);
     });
-}
 
-//ca marche pas
-// function getConnectedUserList() {
-//     let connect_user_list = [];
-//     connected_users_db.find({}, (err, users) => {
-//         if (err) throw err;
-//         users.forEach((user) => connect_user_list.push(user?.name));
-//     })
-//     return connect_user_list;
-// }
-
-server.on('connection', socket => {
-    socket.on('identify', (user) => {
-        //Set him to online in bdd
-        setConnectedUser(user);
-        socket.user = user;
-        // const connect_user_list = getConnectedUserList();
-        // socket.emit('connected user list', connect_user_list);
-    })
     socket.on('disconnect', () => {
-        if (!socket.user)
-            return;
-        //Set him to offline in bdd
-        removeConnectedUser(socket.user);
+        STATIC_CHANNELS.forEach(c => {
+            let index = c.sockets.indexOf(socket.id);
+            if (index != (-1)) {
+                c.sockets.splice(index, 1);
+                c.participants--;
+                io.emit('channel', c);
+            }
+        });
     });
+
+});
+
+
+
+/**
+ * @description This methos retirves the static channels
+ */
+app.get('/getChannels', (req, res) => {
+    res.json({
+        channels: STATIC_CHANNELS
+    })
 });
