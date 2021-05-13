c7ac23785809c8cd26a58e9866c4a1f37dd0a77f
remi <remi.bisson@epitech.eu>
2021-05-13 18:14:40 +0200

    [+] Chat: new button to send message

--- front/src/components/UsersList/UsersList.jsx
+++ front/src/components/UsersList/UsersList.jsx
@@ -50,7 +50,7 @@ export default function UsersList(props) {
                         </div>
                         <div>
                             <button onClick={() => {newMessage()}} type="button" className="inline-flex items-center px-3 py-1.5 border border-transparent shadow-sm text-sm leading-4 rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                               Nouveau message
+                                New message
                                 <svg className="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                     <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"/>
                                     <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"/>
