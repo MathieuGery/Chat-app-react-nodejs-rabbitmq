3d7bde239c9242c46d943998eecc42c0490c5f76
mathieugery <mathieu.gery@epitech.eu>
2021-05-13 20:18:42 +0200

    [Chat server] CORS errors

--- back/chat/index.js
+++ back/chat/index.js
@@ -28,7 +28,7 @@ let STATIC_CHANNELS = [{
 
 app.use(cors());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Credentials', false);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Origin', '*');
     next();
 })
