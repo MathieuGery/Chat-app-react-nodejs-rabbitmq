f9d2a44bb1823c033034531bd2467217e7c507c1
mathieugery <mathieu.gery@epitech.eu>
2021-05-13 19:41:29 +0200

    [Front] Update base chat server url

--- front/src/helpers/socket.js
+++ front/src/helpers/socket.js
@@ -1,5 +1,5 @@
 import socketIOClient from "socket.io-client";
-const ENDPOINT = "http://localhost:8080";
+const ENDPOINT = "https://chatsocket-modern-js.herokuapp.com";
 const socket = socketIOClient(ENDPOINT);
 
 function getChatMessageSocket(setMessage) {
