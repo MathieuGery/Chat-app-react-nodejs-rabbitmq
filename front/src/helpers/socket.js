9657fb4e767a73cbaeb96ac49d98bdc91234c9c4
mathieugery <mathieu.gery@epitech.eu>
2021-05-14 00:17:48 +0200

    [Chat server] Change url chat server

--- front/src/helpers/socket.js
+++ front/src/helpers/socket.js
@@ -1,5 +1,5 @@
 import socketIOClient from "socket.io-client";
-const ENDPOINT = "https://chatsocket-modern-js.herokuapp.com";
+const ENDPOINT = "https://toto.pickeat.fr";
 const socket = socketIOClient(ENDPOINT);
 
 function getChatMessageSocket(setMessage) {
