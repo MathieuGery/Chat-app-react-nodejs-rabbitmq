cbd0696802e705676acb837840698d39c82f7f8e
remi <remi.bisson@epitech.eu>
2021-05-15 17:36:08 +0200

    [*] change room function for roomID

--- front/src/components/ChatList/index.js
+++ front/src/components/ChatList/index.js
@@ -1,30 +1,5 @@
 import {useEffect, useState} from "react";
 import listRooms from "../../requests/Rooms/list";
-import Cookies from "js-cookie";
-import handleAxiosResponseError from "../../helpers/handleAxiosResponseError";
-
-const rooms = [
-    {
-        name: 'Jane Cooper',
-        id: 1,
-        title: 'Regional Paradigm Technician',
-        role: 'Admin',
-        email: 'janecooper@example.com',
-        telephone: '+1-202-555-0170',
-        imageUrl:
-            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-    },
-    {
-        name: 'Jane Cooper',
-        id: 2,
-        title: 'Regional Paradigm Technician',
-        role: 'Admin',
-        email: 'janecooper@example.com',
-        telephone: '+1-202-555-0170',
-        imageUrl:
-            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-    },
-]
 
 function ChatList(props) {
     const [roomsList, setRoomsList] = useState([])
@@ -33,12 +8,15 @@ function ChatList(props) {
         listRooms().then((response) => {
             if (response.success) {
                 setRoomsList(response.success.rooms);
-            } else if (response.warning)
-                return
+            }
         }).catch((error) => {
-            return
+            console.log(error);
         });
     }, []);
+
+    const changeRoom = (roomID) => {
+        props.setRoomId(roomID);
+    }
 
     return (
         <div className="mx-2 overflow-y-auto">
@@ -46,17 +24,13 @@ function ChatList(props) {
                 {roomsList.map((room, index) => (
                     <li key={index} className="px-6 py-5 relative">
                         <div className="group flex justify-between items-center">
-                            <a href="#" className="-m-1 p-1 block">
+                            <a href="#" className="-m-1 p-1 block" onClick={() => {changeRoom(room._id)}}>
                                 <div className="absolute inset-0 group-hover:bg-gray-50" aria-hidden="true"/>
                                 <div className="flex-1 flex items-center min-w-0 relative">
-                    <span className="flex-shrink-0 inline-block relative">
-                      <img className="h-10 w-10 rounded-full"
-                           src='https://picsum.photos/200/300'
-                           alt=""/>
-                          <span
-                              className="bg-green-400 absolute top-0 right-0 block h-2.5 w-2.5 rounded-full ring-2 ring-white"
-                              aria-hidden="true"/>
-                    </span>
+                                    <span className="flex-shrink-0 inline-block relative">
+                                        <img className="h-10 w-10 rounded-full" src='https://picsum.photos/200/300' alt=""/>
+                                        <span className="bg-green-400 absolute top-0 right-0 block h-2.5 w-2.5 rounded-full ring-2 ring-white" aria-hidden="true"/>
+                                    </span>
                                     <div className="ml-4 truncate">
                                         <p className="text-sm font-medium text-gray-900 truncate">{room.name}</p>
                                         <p className="text-sm text-gray-500 truncate">{room.creator}</p>
--- front/src/components/Home/index.js
+++ front/src/components/Home/index.js
@@ -32,9 +32,7 @@ function Home() {
                             <Logo/>
                         </div>
                         <div>
-                            <button type="button" className="-mr-3 h-12 w-12 inline-flex items-center justify-center bg-indigo-600 rounded-md text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"  onClick={() => {
-                                setIsOpen(true);
-                            }}>
+                            <button type="button" className="-mr-3 h-12 w-12 inline-flex items-center justify-center bg-indigo-600 rounded-md text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"  onClick={() => {setIsOpen(true);}}>
                                 <span className="sr-only">Open sidebar</span>
                                 <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -60,8 +58,6 @@ function Home() {
                 </main>
             </div>
         </div>
-
-
 );
 }
 
