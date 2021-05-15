ba25c19a55cc26ac6c797bfff340e1cc5c3b747e
remi <remi.bisson@epitech.eu>
2021-05-15 16:27:00 +0200

    [*] updated sidebar for mobile

--- front/src/components/Home/index.js
+++ front/src/components/Home/index.js
@@ -4,6 +4,7 @@ import SideBarDesktop from "../SideBar/SideBarDesktop";
 import ChatList from "../ChatList";
 import Chat from "../Chat";
 import {getChatMessageSocket, identifyUserChatSocket} from "../../helpers/socket";
+import Logo from "../utils/Logo";
 
 
 function Home() {
@@ -29,7 +30,7 @@ function Home() {
                 <div className="lg:hidden">
                     <div className="bg-indigo-600 py-2 px-4 flex items-center justify-between sm:px-6 lg:px-8">
                         <div>
-                            <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white" alt="Workflow"/>
+                            <Logo/>
                         </div>
                         <div>
                             <button type="button" className="-mr-3 h-12 w-12 inline-flex items-center justify-center bg-indigo-600 rounded-md text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"  onClick={() => {
