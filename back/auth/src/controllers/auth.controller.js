bb8768495c7efce60738f4f4085d4dab66f670b5
mathieugery <mathieu.gery@epitech.eu>
2021-05-16 17:28:33 +0200

    oskour

--- back/auth/src/controllers/auth.controller.js
+++ back/auth/src/controllers/auth.controller.js
@@ -51,8 +51,10 @@ exports.recoverPasswordFinal = async (req, res, next) => {
     if (!req.query.key || !req.body.password) return res.status(httpStatus.BAD_REQUEST)
     const user = await User.findAndRecoverPasswordFinal({activationKey: req.query.key})
     const newPasswordHash = bcrypt.hashSync(req.body.password)
-    user.password = newPasswordHash;
-    user.save()
+    await User.findOneAndUpdate(
+        { 'activationKey': req.query.key },
+        { $set: { 'password': newPasswordHash, 'activationKey': '' } }
+    )
     mailSender.mailConfirmNewPassword(next, user.email)
     return res.json({ message: 'Your password has been successfully reset' })
   } catch (error) {
