5869811bf50ae56c268a9772b61750c35ff6302f
mathieugery <mathieu.gery@epitech.eu>
2021-05-17 12:11:58 +0200

    [Chat Server] Add message to the db

--- back/chat/mongoose.js
+++ back/chat/mongoose.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const MongoClient = require('mongodb').MongoClient;
 const uri = process.env.MONGOURI;
 
-let users_db;
+let users_db, rooms_db;
 
 exports.connect = function () {
     const mongoose = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
@@ -11,6 +11,7 @@ exports.connect = function () {
         if (err) throw err;
         let dbo = db.db("myFirstDatabase");
         users_db = dbo.collection("users");
+        rooms_db = dbo.collection("rooms");
         console.log('MongoDB is connected')
     });
     return mongoose;
@@ -21,6 +22,18 @@ exports.setConnectedUser = function (username, status)  {
     users_db?.findOneAndUpdate({name: username}, {$set: update}, function (err, res) {
         if (!res.value) {
             console.log("No user found");
+        }
+        if (err)  {
+            throw err;
+        }
+    });
+}
+
+exports.addNewMessages = function (message, roomName)  {
+    console.log(message)
+    rooms_db?.findOneAndUpdate({name: roomName}, {$push: {messages: message}}, function (err, res) {
+        if (!res.value) {
+            console.log("No Room found");
         }
         if (err)  {
             throw err;
--- back/chat/src/sendMessage.js
+++ back/chat/src/sendMessage.js
@@ -1,3 +1,6 @@
+const mongoose = require('../mongoose');
+
 module.exports = function sendMessage(exchange, message) {
+    mongoose.addNewMessages(message, exchange.name)
     exchange.pub("", message)
 }
